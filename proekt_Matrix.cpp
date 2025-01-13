175d1f723d5a31f044a96c967523f296135db383
Kristina2607 <krisi260705@gmail.com>
2025-01-13 23:06:35 +0200

    Update proekt_Matrix.cpp

--- proekt_Matrix.cpp
+++ proekt_Matrix.cpp
@@ -2,13 +2,13 @@
 #include <cstdlib>
 #include <ctime>
 #include <windows.h> 
+#include <fstream>
+#include "save_in_file.cpp"
 
 const int MIN_Rows = 4;
 const int MIN_Cols = 4;
 const int MAX_Value = 15;
 char signs[] = { '+', '-', 'x', '/' };
-int resultOfPlayer1 = 0;
-int resultOfPlayer2 = 0;
 
 struct TableElements
 {
@@ -24,6 +24,16 @@ TableElements** createMatrix (int rows, int cols)
 		matrix[i] = new TableElements[cols];
 	}
 	return matrix;
+}
+
+unsigned** createVisitedMatrix(int rows, int cols)
+{
+	unsigned** visitedMatrix = new unsigned* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		visitedMatrix[i] = new unsigned[cols];
+	}
+	return visitedMatrix;
 }
 
 void fillWithRandomValues(TableElements** matrix, int rows, int cols)
@@ -47,9 +57,7 @@ void fillWithRandomValues(TableElements** matrix, int rows, int cols)
 }
 void configureSpecialCells(TableElements** matrix, int rows, int cols)
 {
-	TableElements requiredFieldsValues[] = {
-		{'x', 0}, {'x', 2}, {'/', 2}
-	};
+	TableElements requiredFieldsValues[] = {{'x', 0}, {'x', 2}, {'/', 2}};
 	int requiredFieldsCount = 3;
 
 	for (int i = 0; i < requiredFieldsCount; i++)
@@ -61,12 +69,24 @@ void configureSpecialCells(TableElements** matrix, int rows, int cols)
 	}	
 }
 
-void printMatrix(TableElements** matrix, int rows, int cols)
+void printMatrix(TableElements** matrix, unsigned** visitedMatrix, int rows, int cols, HANDLE hConsole)
 {
 	for (int i = 0; i < rows; i++)
 	{
 		for (int j = 0; j < cols; j++)
 		{
+			if (visitedMatrix[i][j] == 1)
+			{
+				SetConsoleTextAttribute(hConsole, 9); // Син за играч 1
+			}
+			else if (visitedMatrix[i][j] == 2)
+			{
+				SetConsoleTextAttribute(hConsole, 12); // Червен за играч 2
+			}
+			else 
+			{
+				SetConsoleTextAttribute(hConsole, 7); // Бял за непосетени
+			}
 			std::cout << matrix[i][j].sign << matrix[i][j].number << ' ';
 		}
 		std::cout << std::endl;
@@ -91,11 +111,14 @@ bool isValidMove(Position currP, Position newP, int rows, int cols)
 			{
 				if (j == newP.y && j >= 0 && j < cols)
 				{
-					if (i == currP.x && j == currP.y) {
+					if (i == currP.x && j == currP.y)
+					{
 						return false;
 					}
 					else 
+					{
 						return true;
+					}
 				}
 			}
 		}
@@ -103,8 +126,12 @@ bool isValidMove(Position currP, Position newP, int rows, int cols)
 	return false;
 }
 
+
 void addToSumOfPlayer(TableElements** matrix, Position player)
 {
+	int resultOfPlayer1 = 0;
+	int resultOfPlayer2 = 0;
+
 	if (matrix[player.x][player.y].sign == '+') {
 		resultOfPlayer1 += matrix[player.x][player.y].number;
 	}
@@ -117,6 +144,11 @@ void addToSumOfPlayer(TableElements** matrix, Position player)
 	else if (matrix[player.x][player.y].sign == '/') {
 		resultOfPlayer1 /= matrix[player.x][player.y].number;
 	}
+}
+
+bool isAccessible(unsigned** visitedMatrix, int rows, int cols, Position& player)
+{
+	return visitedMatrix[player.x][player.y] == 0;
 }
 
 void movePlayer(TableElements** matrix, int rows, int cols, Position& player)
@@ -136,6 +168,11 @@ void movePlayer(TableElements** matrix, int rows, int cols, Position& player)
 	}
 }
 
+bool isWinner()
+{
+
+}
+
 void deleteMatrix(TableElements** matrix, int rows)
 {
 	for (int i = 0; i < rows; i++)
@@ -151,32 +188,63 @@ int main()
 	int grid_length, grid_width;
 	std::cout << "Input grid size to start the game:";
 	bool validGridSize = false;
-	while (!validGridSize) {
+	while (!validGridSize)
+	{
 		std::cin >> grid_length >> grid_width;
-		if (grid_length >= 4 || grid_width >= 4) {
+		if (grid_length >= 4 || grid_width >= 4) 
+		{
 			validGridSize = true;
 		}
-		else {
+		else 
+		{
 			std::cout << "The matrix has to be at least 4x4! Try again!";
 		}
 	}		
+
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	TableElements** matrix = createMatrix(grid_length, grid_width);
+	unsigned** visitedMatrix = createVisitedMatrix(grid_length, grid_width);
+	for (int i = 0; i < grid_length; i++)
+	{
+		for (int j = 0; j < grid_width; j++)
+		{
+			visitedMatrix[i][j] = 0;  // Всички полета започват като непосетени 
+		}
+	}       
+
 	Position player1 = {0,0};
 	Position player2 = { grid_length - 1, grid_width - 1 };
+
+	visitedMatrix[player1.x][player1.y] = 1; // Играч 1
+	visitedMatrix[player2.x][player2.y] = 2; // Играч 2
+
 	
 	fillWithRandomValues(matrix, grid_length, grid_width);
 	configureSpecialCells(matrix, grid_length, grid_width);
-       /* HANDLE  hConsole;
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 6);
-	std::cin.get();*/
 
-	printMatrix(matrix, grid_length, grid_width);
-	movePlayer (matrix, grid_length, grid_width, player1);
-	movePlayer(matrix, grid_length, grid_width, player2);
+	const char* inputFile = "matrix.txt";
+	int** matrix = readMatrix(inputFile, grid_length, grid_width);
+   
+	//Основен цикъл за протичане на играта
+	while (true)
+	{
+		printMatrix(matrix, visitedMatrix, grid_length, grid_width, hConsole);
+		std::cout << "Player 1, enter your move (row,column): ";
+		if (isAccessible(visitedMatrix, grid_length, grid_width, player1))
+		{
+			visitedMatrix[player1.x][player1.y] = 1;
+			movePlayer(matrix, grid_length, grid_width, player1);
+		}
+		if (isAccessible(visitedMatrix, grid_length, grid_width, player2))
+		{
+			visitedMatrix[player2.x][player2.y] = 1;
+			movePlayer(matrix, grid_length, grid_width, player2);
+		}
 
+	}
+	printMatrix(matrix,visitedMatrix, grid_length, grid_width, hConsole);
 
 	deleteMatrix(matrix, grid_length);
-	Position p;
+	//Position p;
 	return 0;
 }
